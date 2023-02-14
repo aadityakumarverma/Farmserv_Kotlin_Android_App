63ed12febcab23170a3f45fa3b181680e7342ae6
Aaditya <raushankv217@gmail.com>
2023-02-14 23:43:22 +0530

    Initial Commit

--- app/src/main/java/com/example/farmserv/UserLogin.kt
+++ app/src/main/java/com/example/farmserv/UserLogin.kt
@@ -3,9 +3,9 @@ package com.example.farmserv
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class UserLogin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.user_login)
     }
 }
